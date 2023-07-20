22aeb02495bfa930a61c394d8b687d1913c24b3e
DrNickker <mintagirov2015@yandex.ru>
2023-07-20 14:05:25 +0500

    axios

--- src/hooks/useJsonFetch.js
+++ src/hooks/useJsonFetch.js
@@ -1,15 +1,56 @@
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { PropTypes } from "prop-types";
 
-export default function useJsonFetch(url, initial) {
-    const [data, setData] = useState(initial)
+async function useRequest(url, { opts, postData }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(rates => {
-                setData(rates);
-            });
-    }, [url]);
+  const makeGetRequest = async (url) => {
+    try {
+      setIsLoading(true);
+      const responce = await axios.get(url);
+      setData(responce.data);
+    } catch (err) {
+      console.error(err);
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return [data];
+  const makePostRequest = async (url, data) => {
+    await axios
+      .post(url, data)
+      .then((responce) => setData(responce.data))
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        window.location.href = process.env.REACT_APP_HOME_URL;
+      });
+  };
+
+  useEffect(() => {
+    if (opts === "posts") {
+      makeGetRequest(url + opts);
+    }
+    if (opts === "DELETE") {
+      console.log("delete request");
+    }
+    if (opts === "POST") {
+      makePostRequest(url, postData);
+    }
+  }, [opts]);
+
+  return [{ data, isLoading, error }];
 }
+
+useRequest.propTypes = {
+  url: PropTypes.string.isRequired,
+  opts: PropTypes.string.isRequired,
+};
+
+export default useRequest;

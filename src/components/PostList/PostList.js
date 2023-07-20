8952158e66a27aa0d4fe224dfd8ce31728a28e82
DrNickker <mintagirov2015@yandex.ru>
2023-07-20 14:29:39 +0500

    add_button

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,7 +1,5 @@
 import "./PostList.css";
-import { PropTypes } from 'prop-types';
-
-// components
+import PropTypes from 'prop-types';
 import ButtonSave from "../ui-elems/ButtonSave";
 import PostCard from "../PostCard/PostCard";
 
